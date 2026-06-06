bc62533c95949a27a94b2c696a732dc17c4c2134
Soumyadeep Pati <soumyadeeppati777@gmail.com>
2026-06-07 02:20:53 +0530

    0

--- crops/urls.py
+++ crops/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import dashboard, home, add_soil_data, dashboard
+from .views import dashboard, home, add_soil_data, dashboard    
 
 urlpatterns = [
     path('', home, name='home'),
