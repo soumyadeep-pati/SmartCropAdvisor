a6e9adde2a50f39222e598355c5e23528fb08785
Soumyadeep Pati <soumyadeeppati777@gmail.com>
2026-06-07 02:28:17 +0530

    Add user registration functionality

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def signup_view(request):
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request,
+        'accounts/signup.html',
+        {'form': form}
+    )
